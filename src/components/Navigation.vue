98b1ceaa5698c55dccb8e190b7e3c792843786f3
Ekoban <robinet.alexandre@gmail.com>
2018-11-14 10:29:52 +0100

    Router

---
+++ src/components/Navigation.vue
@@ -0,0 +1,42 @@
+<template>
+    <div>
+        <nav class="main-nav">
+            <ul>
+                <li><router-link :to="{ name: 'Home' }">Home</router-link></li>
+                <li><router-link :to="{ name: 'About' }">About</router-link></li>
+            </ul>
+            <h2>User Profiles</h2>
+            <ul>
+                <li v-for="(id, index) in userIds" :key="index">
+                    <router-link :to="{ name: 'ViewProfile',  params: { user_id: id }}">   
+                        <span>Profile {{ id }}</span>
+                    </router-link>
+                </li>
+            </ul>
+        </nav>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'Navigation',
+    data() {
+        return {
+            userIds: ['1', '2', '3', '4'],
+            searchTerm: ''
+        }
+    }
+}
+</script>
+
+<style scoped>
+ul {
+    list-style-type: none;
+    padding: 0;
+}
+a{
+    color: #42b983;
+}
+
+</style>
+
---
+++ src/components/ViewProfile.vue
@@ -0,0 +1,29 @@
+<template>
+    <div class="view-profile">
+        <h2>Profile for {{ userId }}</h2>    
+    </div>    
+</template>
+
+<script>
+export default {
+    name: 'ViewProfile',
+    data () {
+        return {
+            userId: this.$route.params.user_id
+        }
+    },
+    watch: { // pour surveiller un élément et fire un event
+        $route: 'updateId' //cf l'objet route dans router.js
+    },
+    methods : {
+        updateId() {
+            this.userId = this.$route.params.user_id
+        }
+    }
+}
+</script>
+
+<style scoped>
+
+</style>
+ 
